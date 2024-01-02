166f443bd782c7af270f6915c748e2591797510a
Chriford Siame <https://chriford@bitbucket.org>
2024-01-03 01:21:24 +0200

    tailwind css configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,tsx}",
+    "./src/App.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
