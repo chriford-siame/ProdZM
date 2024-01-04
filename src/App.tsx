fa821009aed1d89653cd8397d6594eef5f1b5a88
Chriford Siame <https://chriford@bitbucket.org>
2024-01-04 23:33:57 +0200

    modify file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import PopOver from './components/PopOver';
 import { ScrollArea } from './components/ui/scroll-area';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import ProductEdit from './components/ProductEdit';
 import NoPage from './components/NoPage';
+import ProductCreate from './components/ProductCreate';
+import ProductStatus from './components/ProductStatus';
+import ProductDelete from './components/ProductDelete';
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
         <Route path="/" element={<ProductList />} />
         <Route path="/product/detail" element={<ProductDetail />} />
         <Route path="/product/edit" element={<ProductEdit />} />
+        <Route path="/product/delete" element={<ProductDelete />} />
+        <Route path="/product/create" element={<ProductCreate />} />
+        <Route path="/product/status" element={<ProductStatus />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
