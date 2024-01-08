d34d40485ac4232707a17499d5e861bde73f2232
Chriford Siame <https://chriford@bitbucket.org>
2024-01-08 04:09:02 +0200

    configure oauth

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,18 @@ import './index.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { env } from 'process';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="793744695975-7mcj1857jd9ieomtvauim07vrvvbpt9j.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>;
   </React.StrictMode>
 );
-// reportWebVitals(console.log)
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
