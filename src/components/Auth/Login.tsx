dd7d57152a7b09c35da4bdfaeccb14068d45328c
Chriford Siame <143299729+chriford-siame@users.noreply.github.com>
2024-01-08 04:13:47 +0200

    Create Auth Screens (#8)
    * Copy Components
    
    * Paste Component
    
    * Remove Imports
    
    * Refactor Imports
    
    * Insert a staticfile
    
    * Create and style the registration page
    
    * Create a login page
    
    * install new dependencies
    
    * configure oauth
    
    * Create the Login page
    
    * Create the Signup page
    
    ---------
    
    Co-authored-by: Chriford Siame <https://chriford@bitbucket.org>

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,7 +1,59 @@
 import React from 'react'
+import { GoogleLogin } from '@react-oauth/google'
+import { Button } from "../ui/button"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { jwtDecode } from 'jwt-decode'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='w-screen h-full flex items-center justify-center py-5'>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle className=' self-center'>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email address</Label>
+                <Input id="email" type={'email'} placeholder="example@email.com" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input type={'password'} id="password" placeholder="******" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="grid gap-1">
+          <Button className='w-full' type='submit'>Submit</Button>
+          <div className='flex justify-center'>
+            <p>Don't have an account? <a href='/signup' className=' text-blue-400 hover:text-blue-500'>Signup</a></p>
+          </div>
+          <div className='flex justify-between items-center'>
+            <span className='border w-full mr-1 h-[1px]'></span>
+            <span>Or</span>
+            <span className='border w-full ml-1 h-[1px]'></span>
+          </div>
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              const credentialResponseDecoded = jwtDecode(`${credentialResponse.credential}`);
+              console.log(credentialResponseDecoded)
+            }}
+            onError={() => { 
+              console.log("Failed to login") 
+            }} 
+          /> 
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
