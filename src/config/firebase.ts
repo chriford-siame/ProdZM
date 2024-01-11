dd91ec195f334968b8bf05ad37aa89d51cdd01e9
Chriford Siame <https://chriford@bitbucket.org>
2024-01-11 23:33:05 +0200

    comment code

---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import firebase from 'firebase'
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBIJyfDE7Rhx--DB6AneLUJFGhpb5nHurY",
+//   authDomain: "prodzm-e12ff.firebaseapp.com",
+//   projectId: "prodzm-e12ff",
+//   storageBucket: "prodzm-e12ff.appspot.com",
+//   messagingSenderId: "1076142985370",
+//   appId: "1:1076142985370:web:f5e2e35a1b97a5474fcf23",
+//   measurementId: "G-7W8ZZCLS86"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// export default firebase;
+export {}
