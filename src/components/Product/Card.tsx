c4013790206f6269843408fc47db38359e4eb26c
Chriford Siame <https://chriford@bitbucket.org>
2024-01-12 23:48:45 +0200

    Update the buttons

--- src/components/Product/Card.tsx
+++ src/components/Product/Card.tsx
@@ -8,7 +8,7 @@ import {
     CardTitle,
 } from "../ui/card"
 import { Button } from '../ui/button'
-import { FaBookmark, FaEye, FaStar } from 'react-icons/fa'
+import { FaCartPlus, FaEye, FaStar } from 'react-icons/fa'
 import { useNavigate } from "react-router-dom";
 
 
@@ -29,7 +29,7 @@ export default function ProductCard() {
     }
 
     return (
-        <Card className=' shadow-xl hover:shadow-2xl'>
+        <Card className='hover:-mt-1 shadow-xl hover:shadow-2xl'>
             <img className='w-full' src={require('src/assets/images/product/product-1.png')} alt="" />
             <CardHeader className=' items-center pt-1'>
                 <div className='border-t mx-10 w-full'></div>
@@ -46,10 +46,10 @@ export default function ProductCard() {
                 </div>
                 <div className='flex justify-between px-1  text-[#7c7a7a] text-sm'>
                     <p>Orders:</p>
-                    <p>$120.00</p>
+                    <p>1200+</p>
                 </div>
                 <div className='flex justify-between px-1  text-[#7c7a7a] text-sm'>
-                    <p>Items:</p>
+                    <p>Remaining:</p>
                     <p>100+</p>
                 </div>
                 <div className='flex justify-between px-1  text-[#7c7a7a] text-sm'>
@@ -65,7 +65,7 @@ export default function ProductCard() {
             </CardContent>
             <div className='pt-10 flex gap-2 justify-center items-center p-2'>
                 <Button onClick={() => nextPath(1)} className='  rounded-t-sm rounded-md rounded-r-lg bg-[#66b641] hover:bg-[#6bc541] w-full gap-1'><FaEye size={18} color='gray' /> Preview</Button>
-                <Button className=' text-white flex items-center justify-center bg-[#d3cfcf] hover:bg-[#bdb8b8]'><FaBookmark color='gray' /></Button>
+                <Button className=' text-white flex items-center justify-center bg-[#d3cfcf] hover:bg-[#bdb8b8]'><FaCartPlus size={18} color='gray' /></Button>
             </div>
         </Card>
     )
