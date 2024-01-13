054daae841f9c32c70765587bbe8a797390c1445
Chriford Siame <https://chriford@bitbucket.org>
2024-01-13 20:48:00 +0200

    A toast messages

--- src/components/Product/Card.tsx
+++ src/components/Product/Card.tsx
@@ -10,34 +10,34 @@ import {
 import { Button } from '../ui/button'
 import { FaCartPlus, FaEye, FaStar } from 'react-icons/fa'
 import { useNavigate } from "react-router-dom";
+import { useToast } from '../ui/use-toast';
 
 
 interface Product {
     name: string;
     description: string;
-    
+
 }
 interface IProps {
     product: string;
 }
 export default function ProductCard() {
+    const { toast } = useToast()
     const navigate = useNavigate();
-
     const nextPath = (id: string | number) => {
         let nextPath = `/product/detail/${id}`;
-        navigate(nextPath); 
+        navigate(nextPath);
     }
 
     return (
         <Card className='hover:-mt-1 shadow-xl hover:shadow-2xl'>
-            <img className='w-full' src={require('src/assets/images/product/product-1.png')} alt="" />
+            <img className='w-full h-56' src={require('src/assets/images/product/tshirt-1.png')} alt="" />
             <CardHeader className=' items-center pt-1'>
                 <div className='border-t mx-10 w-full'></div>
                 <CardTitle className=' text-[#7c7a7a]'>
-                    <a href='/product/detail'>Watch</a>
+                    <a href='/product/detail'>Tshirt</a>
                 </CardTitle>
                 <div className='border-t mx-10 w-full'></div>
-                {/* <CardDescription>Card Description</CardDescription> */}
             </CardHeader>
             <CardContent className='w-full pb-0'>
                 <div className='flex justify-between px-1  text-[#7c7a7a] text-sm'>
@@ -53,6 +53,10 @@ export default function ProductCard() {
                     <p>100+</p>
                 </div>
                 <div className='flex justify-between px-1  text-[#7c7a7a] text-sm'>
+                    <p>Shipping:</p>
+                    <p>60 days</p>
+                </div>
+                <div className='flex justify-between px-1  text-[#7c7a7a] text-sm'>
                     <p className='items-center'>Rating:</p>
                     <p className='items-center flex'>
                         <FaStar size={12} color='yellow' />
@@ -65,7 +69,13 @@ export default function ProductCard() {
             </CardContent>
             <div className='pt-10 flex gap-2 justify-center items-center p-2'>
                 <Button onClick={() => nextPath(1)} className='  rounded-t-sm rounded-md rounded-r-lg bg-[#66b641] hover:bg-[#6bc541] w-full gap-1'><FaEye size={18} color='gray' /> Preview</Button>
-                <Button className=' text-white flex items-center justify-center bg-[#d3cfcf] hover:bg-[#bdb8b8]'><FaCartPlus size={18} color='gray' /></Button>
+                <Button onClick={() => toast({
+                    title: "Save For Later",
+                    description: "Item saved to cart successfully",
+                    duration: 3500,
+                    
+                })}
+                    className=' text-white flex items-center justify-center bg-[#d3cfcf] hover:bg-[#bdb8b8]'><FaCartPlus size={18} color='gray' /></Button>
             </div>
         </Card>
     )
