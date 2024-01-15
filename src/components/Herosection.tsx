df3fa8cb87f391cf3bc4806fd5303a8aaa07a94e
Chriford Siame <https://chriford@bitbucket.org>
2024-01-15 13:36:36 +0200

    Delete File

--- src/components/Herosection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Herosection() {
-  return (
-    <div className='w-screen'>
-        <img className='w-screen' src={require('src/assets/app/heroSection.png')} alt="" />
-    </div>
-  )
-}
