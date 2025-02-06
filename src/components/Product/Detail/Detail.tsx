09ad0474eb1418c077be69a1be589036bf4eda66
chriford-siame <chrifordsiame@gmail.com>
2025-02-06 23:12:52 +0200

    Display dynamic data

--- src/components/Product/Detail/Detail.tsx
+++ src/components/Product/Detail/Detail.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Card } from '../../ui/card';
 import { Button } from '../../ui/button';
 import ShippingDetailFormModal from './ShippingDetailForm';
 import { DollarSign, Edit2Icon, GlobeIcon, ListChecksIcon, ListMinus, ListTodo, LocateIcon, MapPinIcon, PhoneIcon, Star, UserIcon } from 'lucide-react';
 import DetailCard from './Card';
+import useProduct from '../../../hooks/Product';
+import { IProductImage } from '@/interfaces/products';
 
 export default function ProductDetail() {
-  const { id } = useParams();
+  const { id } = useParams<{id: string}>();
+  const productId = id ? parseInt(id, 10) : 0;
+  const { product, loading, error } = useProduct(productId);
+  const [secondaryImage, setSecondaryImage] = useState<string | null>(null);
+  
+  
+  if (error) return <p>{error}</p>;
+  if (!product) return <p></p>;
+  
+  const mainImage = product.images.find((img: IProductImage) => img.is_main);
 
   return (
     <React.Fragment>
-      <div className='mb-4 mx-10'>
+      <div className='mb-4'>
         <div className='flex mx-6 mb-4 justify-between py-1 border-b'>
           <div className='flex pr-2'>
             <a href="/" className=' flex text-[#3da3aa] rounded-lg px-2 bg-slate-200 hover:bg-slate-300'>back</a>
@@ -27,21 +38,17 @@ export default function ProductDetail() {
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-2'>
               <div className='pr-4'>
                 <div className="rounded-md">
-                  <img src={require("src/assets/images/product/tshirt-2.png")} alt="" />
+                  <img className="max-h-96 min-h-96 min-w-16 max-w-80" src={secondaryImage ? secondaryImage : mainImage?.image} alt="" />
                 </div>
                 <div className='flex gap-3 justify-center mt-2'>
-                  <button className=' border border-slate-300'>
-                    <div className="h-6 w-6 rounded-md bg-[url('src/assets/images/product/tshirt-4.png')] bg-cover bg-no-repeat"></div>
-                  </button>
-                  <button className=' border border-slate-300'>
-                    <div className="h-6 w-6 rounded-md bg-[url('src/assets/images/product/tshirt-4.png')] bg-cover bg-no-repeat"></div>
-                  </button>
-                  <button className=' border border-slate-300'>
-                    <div className="h-6 w-6 rounded-md bg-[url('src/assets/images/product/tshirt-4.png')] bg-cover bg-no-repeat"></div>
-                  </button>
-                  <button className=' border border-slate-300'>
-                    <div className="h-6 w-6 rounded-md bg-[url('src/assets/images/product/tshirt-4.png')] bg-cover bg-no-repeat"></div>
-                  </button>
+                  {product.images.map((media: IProductImage) => {
+                    return (
+                      <button key={media.id} onMouseEnter={() => setSecondaryImage(media.image)} onMouseLeave={() => setSecondaryImage(null)} className=' border border-slate-300'>
+                        <img className={`h-10 w-10 rounded-md bg-cover bg-no-repeat`} height={20} src={media.image} />
+                      </button>
+                    )
+                  })}
+                  
                 </div>
                 <div className='pb-1 flex gap-1 justify-between'>
                   <Button className=' w-full mt-3 hover:font-bold flex gap-2'> Buy Now</Button>
@@ -50,34 +57,34 @@ export default function ProductDetail() {
               </div>
               <div className='border px-5 rounded-md '>
                 <div className='text-center border-b py-2 font-semibold text-[16pt] text-muted-foreground'>
-                  <p>Tshirt Title</p>
+                  <p>{product.name}</p>
                 </div>
                 <div>
-                  <p className='text-start border-b text-muted-foreground text-[11pt] py-2'>
-                    Briefproductescription
-                    ptiondescriptiondes  criptiond escriptiondescription...
+                  {/* <p className='text-start border-b text-muted-foreground text-[11pt] py-2'>
+                    {product.}
+                    ...
                     read more
-                  </p>
+                  </p> */}
                 </div>
                 <div className='flex justify-between text-muted-foreground text-[12pt] py-2'>
                   <div className='flex gap-1'><ListTodo /> Category</div>
-                  <div>Electronics</div>
+                  <div>{product.category}</div>
                 </div>
                 <div className='flex justify-between text-muted-foreground text-[12pt] py-2'>
                   <div className='flex gap-1'><DollarSign /> Price</div>
-                  <div>$10.00</div>
+                  <div>${product.price}</div>
                 </div>
                 <div className='flex justify-between text-muted-foreground text-[12pt] py-2'>
                   <div className='flex gap-1'><ListChecksIcon /> Orders</div>
-                  <div>812</div>
+                  <div>{product.orders}</div>
                 </div>
                 <div className='flex justify-between text-muted-foreground text-[12pt] py-2'>
                   <div className='flex gap-1'><Star /> Rating</div>
-                  <div>54</div>
+                  <div>{product.rating}</div>
                 </div>
                 <div className='flex justify-between text-muted-foreground text-[12pt] py-2'>
                   <div className='flex gap-1'><ListMinus /> Remaining</div>
-                  <div>54</div>
+                  <div>{product.remaining_stock}</div>
                 </div>
 
               </div>
