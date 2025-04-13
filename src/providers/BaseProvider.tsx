f7d2229c0be8413bab099e44b5043b6fcf92f64a
chriford-siame <chrifordsiame@gmail.com>
2025-04-13 23:51:43 +0200

    Modify context provider

--- src/providers/BaseProvider.tsx
+++ src/providers/BaseProvider.tsx
@@ -7,7 +7,11 @@ interface AppProviderProps {
 
 const BaseProvider: React.FC<AppProviderProps> = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
+  const [user, setUser] = useState([]);
+  
   return <AppContext.Provider value={{
+    user,
+    setUser,
     cartItems,
     setCartItems, 
   }}>{children}</ AppContext.Provider>
